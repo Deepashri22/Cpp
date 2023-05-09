5526db9ddb92b1b3ff61c5d1bb5736036d4d2041
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-05-09 22:31:41 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,32 @@
+/*#include<iostream> 
+using namespace std; 
+int main(){
+    cout<<"hello world"<<endl;
+    cout<<"hello cw"<<"\n";
+    cout<<"hello sandu";
+    return 0;
+}*/
+//dry run- understanding the execution of code with pen and paper 
+//computer does not print or read \n 
+//escape sequence= \n and endl
+//tab= 4 spaces 
+
+//********PRINTING NUMBERS************
+
+/*#include<iostream> 
+using namespace std; 
+int main(){
+    cout<<4<<endl;
+    cout<<4+3<<"\n";
+    cout<<"4+3";
+    return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 7;
+    cout<<x;
+}
+
