6afcde7f0dc49c48eb52d7cdea7ba9a197cd3b09
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-05-10 09:34:54 +0530

    Add files via upload

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    int y = 2;
+    cout<<x+y<<endl;
+    cout<<x/y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+
+    return 0;
+} 
+
+//the output of integers is integer only
+
+#include<iostream> 
+using namespace std;
+int main(){
+    int x = 5;
+    cout<<x<<endl;
+    ++x; //pre- increment
+    cout<<x<<endl;
+    x++; //post - increment - kam ke bad increment 
+    cout<<x<<endl;
+    cout<<++x<<endl; 
+    cout<<--x<<endl;
+    x--,
+    cout<<x<<endl;
+    return 0;
+}
+
