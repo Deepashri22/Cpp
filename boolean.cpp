66debc3914da25bd790c4702937d3084a2904186
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-05-11 23:22:19 +0530

    Add files via upload

---
+++ boolean.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool flag= false;
+    cout<<flag<<endl;
+    //any integer given to bool flag gives the answer as '1', except '0'. 
+    cout<<(7>4)<<endl;
+    cout<<(7<4)<<endl;
+    
+}
