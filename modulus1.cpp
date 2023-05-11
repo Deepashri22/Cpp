86b4a9e98688acb43c3247dd263ae1e59b4e4b13
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-05-11 22:40:42 +0530

    Add files via upload

---
+++ modulus1.cpp
@@ -0,0 +1,43 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    //cout<<2%40;
+    //if a>b, then a%b= reminder
+    //if a=b, then a%b= remainder; 
+    //if a<b, then a%b= a = remainder; 
+    cout<< 99 % (-4)<<endl;
+    cout<< 99%4<<endl; 
+    cout<< 99 % -4<<endl;
+    cout<<-99 % 4<<endl;
+    cout<< -99%-4<<endl;
+    //-a%b= -(a%b)
+    //-a % -b= -(a%b)
+    //a % -b= a%b
+} */
+
+//percentage of 5 subjects
+#include<iostream> 
+using namespace std;
+int main(){
+    /*float x1= 90;
+    float x2= 95;
+    float x3= 98;
+    float x4= 100;
+    float x5= 96;
+    float result = (x1+x2+x3+x4+x5)/5;
+    cout<<result<<endl; */ 
+
+    //area of a circle 
+    float pi = 3.14;
+    float r= 5;
+    float area = pi* r * r; 
+    cout<< area<<endl; 
+
+    //simple interest
+    float p= 1200;
+    float rate= 14;
+    float t=3;
+    float si= (p*r*t)/100;
+    cout<< si<< endl;
+
+}
