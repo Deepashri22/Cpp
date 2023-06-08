22d452a70b99f7e6470640f84cf7ee68bd97f722
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-06-08 14:23:07 +0530

    Add files via upload

---
+++ ullu.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int x=10, y=20;
+    if(x==y);
+        cout<<x<<" "<<y;
+    return 0;*/
+    int x=3, y=5;
+    if(x==3)
+       cout<<x<<endl;
+    else;
+       cout<<y<<endl;
+
+    return 0;
+}
