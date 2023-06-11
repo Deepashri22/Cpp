6d13ff886bb060a9b37df9c75e7ebd4cf17c2269
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-06-11 15:06:26 +0530

    for loop question

---
+++ APQ.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //cout<<"Enter the number of terms you want:";
+    //int n;
+    //cin>>n; 
+    //int a=1;
+    for(int i=100;i>=0;i=i-3){
+        cout<<i<<endl; 
+        //a=a*2;
+    }
+}
