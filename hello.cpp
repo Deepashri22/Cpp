1d0c5f0ef51a9884922277f61d332be089d9409e
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-06-11 12:47:03 +0530

    for loop

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the number of times you want to repeat:"<<endl;
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"Hello World"<<endl;
+    }
+}
